df7c9f57ac53bc098111ec0dafd84d41b3957f80
Giacomo Pasin <giacomo.pasin@gmail.com>
2025-04-30 12:08:09 +0200

    continued with the scraper

--- scripts/sdbs_scraper.py
+++ scripts/sdbs_scraper.py
@@ -1,42 +1,78 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+import urllib.request
+import multiprocessing
+from pathlib import Path
 
-driver = webdriver.Chrome()
+Path("../sdbs_dataset/sdbs_images").mkdir(parents=True, exist_ok=True)
 
-URL = "https://sdbs.db.aist.go.jp/"
-driver.get(URL)
+def download_image(url, save_as):
+    urllib.request.urlretrieve(url, save_as)
 
-title = driver.title
+def get_gif(ids):
+    # start in headless mode
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
 
-driver.set_window_size(1920, 1920)
+    BASE_URL = "https://sdbs.db.aist.go.jp/SearchInformation.aspx"
+    driver.get(BASE_URL)
+    # check if there is an agreement to accept
+    agreement_box = driver.find_element(by=By.CSS_SELECTOR, value=".DisclaimeraAcceptClass")
+    if agreement_box.is_displayed():
+        # scroll to the bottom of the page
+        print("Found agreement to accept")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(1)
+        agreement_button = driver.find_element(by=By.CSS_SELECTOR, value=".DisclaimeraAcceptClass > input")
+        agreement_button.click()
+        print("Agreement accepted")
 
-driver.implicitly_wait(1)
+    driver.set_window_size(1920, 1920)
 
-# check if there is an agreement to accept
-agreement_box = driver.find_element(by=By.CSS_SELECTOR, value=".DisclaimeraAcceptClass")
-if agreement_box.is_displayed():
-    # scroll to the bottom of the page
-    print("Found agreement to accept")
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(1)
-    agreement_button = driver.find_element(by=By.CSS_SELECTOR, value=".DisclaimeraAcceptClass > input")
-    agreement_button.click()
-    print("Agreement accepted")
+    # driver.implicitly_wait(1)
+    for sdbs_no in ids:
 
-sdbs_no_text_box = driver.find_element(by=By.ID, value="BodyContentPlaceHolder_INP_sdbsno")
-search_btn = driver.find_element(by=By.ID, value="BodyContentPlaceHolder_SearchButton")
+        try:
+            sdbs_no_text_box = driver.find_element(by=By.ID, value="BodyContentPlaceHolder_INP_sdbsno")
+            search_btn = driver.find_element(by=By.ID, value="BodyContentPlaceHolder_SearchButton")
 
-sdbs_no = "1"
-sdbs_no_text_box.send_keys(sdbs_no)
-search_btn.click()
+            sdbs_no_text_box.send_keys(sdbs_no)
+            search_btn.click()
 
-ir_data_btn = driver.find_element(by=By.CSS_SELECTOR, value=".ir-val > a")
-ir_data_btn.click()
+            ir_data_btn = driver.find_element(by=By.CSS_SELECTOR, value=".ir-val > a")
+            ir_data_btn.click()
 
-img_element = driver.find_element(by=By.CSS_SELECTOR, value=".MainBody > div > img")
-img_src = img_element.get_attribute("src")
 
-print(sdbs_no, img_src)
+            driver.implicitly_wait(1)
+            img_element = driver.find_element(by=By.CSS_SELECTOR, value=".MainBody > div > img")
+            img_src = img_element.get_attribute("src")
 
-driver.quit()
+            print(sdbs_no, img_src)
+            download_image(img_src, "../sdbs_dataset/sdbs_images/" + sdbs_no + ".gif")
+        except Exception as e:
+            print(e)
+
+        driver.get(BASE_URL)
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+    # get the number of cores in the system
+    cores = multiprocessing.cpu_count() - 1
+
+    ids = []
+    for i in range(cores):
+        ids.append([])
+    ids_file = open("../sdbs_dataset/sdbs_ids.txt", "r")
+
+    for i, id in enumerate(ids_file):
+        ids[i % cores].append(id.strip())
+    ids_file.close()
+
+    for i in range(cores):
+        p = multiprocessing.Process(target=get_gif, args=(ids[i],))
+        p.start()
